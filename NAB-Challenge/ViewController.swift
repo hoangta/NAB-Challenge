2984c395e3220e032f1e02935ea50a4025ebcde3
Hoang Ta <vinhhoang.ta@2359media.com>
2020-07-02 12:04:07 +0700

    initial commit.

---
+++ NAB-Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NAB-Challenge
+//
+//  Created by Hoang Ta on 7/2/20.
+//  Copyright © 2020 ht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ NAB-ChallengeTests/NAB_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  NAB_ChallengeTests.swift
+//  NAB-ChallengeTests
+//
+//  Created by Hoang Ta on 7/2/20.
+//  Copyright © 2020 ht. All rights reserved.
+//
+
+import XCTest
+@testable import NAB_Challenge
+
+class NAB_ChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
