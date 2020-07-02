278471a8ef91e92d8fc6299d128aa4d26c0a3114
Hoang Ta <vinhhoang.ta@2359media.com>
2020-07-02 12:08:43 +0700

    [mod] app restructure.

---
+++ NAB-Challenge/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  NAB-Challenge
+//
+//  Created by Hoang Ta on 7/2/20.
+//  Copyright © 2020 ht. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
